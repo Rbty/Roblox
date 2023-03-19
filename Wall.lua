971080d802a40c8d9b3b9d77dce4d87dca9d3bd6
Rbty <92282813+Rbty@users.noreply.github.com>
2023-03-19 01:11:38 -0300

    Update Wall.lua

--- Wall.lua
+++ Wall.lua
@@ -1,58 +1,57 @@
-local playerName = game.Players.LocalPlayer.Name
-local playerTeam = game.Players.LocalPlayer.Team
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 
 local function createSurfaceGui(player)
-    if player.Team ~= playerTeam then
-        local character = player.Character or player.CharacterAdded:Wait()
-        local humanoid = character:WaitForChild("Humanoid")
-        local rootPart = character:WaitForChild("Head")
+	if player == LocalPlayer then
+		return 
+	end
 
-        local existingGui = rootPart:FindFirstChild("HeadSurfaceGui")
-        if not existingGui then
-            local gui = Instance.new("SurfaceGui")
-            gui.AlwaysOnTop = true
-            gui.Face = Enum.NormalId.Top
-            gui.CanvasSize = Vector2.new(100, 100)
-            gui.Parent = rootPart
+	local character = player.Character or player.CharacterAdded:Wait()
+	local humanoid = character:WaitForChild("Humanoid")
+	local rootPart = character:WaitForChild("Head")
+	
+	local existingGui = rootPart:FindFirstChild("SurfaceGui")
+	if not existingGui then
+		local gui = Instance.new("SurfaceGui")
+		gui.AlwaysOnTop = true
+		gui.Face = Enum.NormalId.Top
+		gui.CanvasSize = Vector2.new(100, 100) 
+		gui.Parent = rootPart
+	
+		local label = Instance.new("TextLabel")
+		label.Size = UDim2.new(1, 0, 1, 0)
+		label.BackgroundColor3 = Color3.new(1, 1, 1)
+		label.TextScaled = true
+		label.BorderSizePixel = 0
+		label.Parent = gui
+		label.Text = ""
+		label.Transparency = 0.9
+		
+		local faces = {Enum.NormalId.Back, Enum.NormalId.Bottom, Enum.NormalId.Front, Enum.NormalId.Left, Enum.NormalId.Right}
+		for _, face in ipairs(faces) do
+			local gui = Instance.new("SurfaceGui")
+			gui.AlwaysOnTop = true
+			gui.Face = face 
+			gui.CanvasSize = Vector2.new(100, 100)
+			gui.Parent = rootPart
 
-            local label = Instance.new("TextLabel")
-            label.Size = UDim2.new(1, 0, 1, 0)
-            label.BackgroundColor3 = Color3.new(1, 1, 1)
-            label.TextScaled = true
-            label.BorderSizePixel = 0
-            label.Parent = gui
-            label.Text = ""
-            label.Transparency = 0.9
-
-            local faces = {Enum.NormalId.Back, Enum.NormalId.Bottom, Enum.NormalId.Front, Enum.NormalId.Left, Enum.NormalId.Right}
-            for _, face in ipairs(faces) do
-                local gui = Instance.new("SurfaceGui")
-                gui.AlwaysOnTop = true
-                gui.Face = face
-                gui.CanvasSize = Vector2.new(100, 100)
-                gui.Parent = rootPart
-
-                local label = Instance.new("TextLabel")
-                label.Size = UDim2.new(1, 0, 1, 0)
-                label.BorderSizePixel = 0
-                label.BackgroundColor3 = Color3.new(1, 1, 1)
-                label.TextScaled = true
-                label.Text = ""
-                label.Parent = gui
-                label.Transparency = 0.9
-            end
-        end
-    end
+			local label = Instance.new("TextLabel")
+			label.Size = UDim2.new(1, 0, 1, 0)
+			label.BorderSizePixel = 0
+			label.BackgroundColor3 = Color3.new(1, 1, 1)
+			label.TextScaled = true
+			label.Text = ""
+			label.Parent = gui
+			label.Transparency = 0.9
+		end
+	end
 end
 
 while true do
-    playerTeam = game.Players.LocalPlayer.Team
-
-    for _, player in ipairs(game:GetService("Players"):GetPlayers()) do
-        if player.Name ~= playerName then
-            createSurfaceGui(player)
-        end
-    end
-
-    wait(3)
+	wait(3)
+	for _, player in ipairs(Players:GetPlayers()) do
+		if player ~= LocalPlayer then
+			createSurfaceGui(player)
+		end
+	end
 end
